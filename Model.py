33e2dc25620bd2d8e54c27d6cc3a40f44dca25c5
Mostafathereal <mohsem1@mcmaster.ca>
2019-09-06 03:51:19 -0400

    model

--- Model.py
+++ Model.py
@@ -1,20 +1,10 @@
-# # import pandas as pd
-# import numpy as np
-# import keras
-# import matplotlib.pyplot as plt
-# from keras.applications import MobileNet
-# from keras.preprocessing import image
-# from keras.applications.mobilenet import preprocess_input
-# from keras.preprocessing.image import ImageDataGenerator
-# from keras.models import Model
-# from keras.optimizers import Adam
+
 
 import json
-import inception
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications import InceptionResNetV2
 from keras.models import Model, model_from_json
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.layers import Dense, Flatten, Dropout
 
 from keras import layers
 from keras import models
@@ -22,7 +12,22 @@ from keras import models
 
 
 
-base_model = InceptionV3(weights='imagenet', include_top = False)
- # print(base_model)
+base_model = InceptionV3(weights='imagenet', include_top = False, input_shape = (256, 256, 3))
 
-print(base_model.summary())
+for layer in base_model.layers[:300]:
+    layer.trainable = False
+ # print(base_model)
+# x = base_model.output
+x = Flatten()(base_model.output)
+x = Dense(128, activation = "relu")(x)
+x = Dropout(0.5)(x)
+x = Dense(512, activation="relu")(x)
+
+## predictions
+x = Dense(2, activation="softmax")(x)
+
+
+
+model = Model(input = base_model.input, output = x)
+
+model.summary()
